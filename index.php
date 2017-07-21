e5249397c50f59b3b00f4b22e8c639c7f66fe1be
gm28sv0rt <ohs70230@gmail.com>
2017-07-22 01:22:58 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>タイトル</title>
+    <link href="C:\Users\a3no_\Desktop\HTML\index.css" rel="stylesheet">
+    </style>
+  </head>
+  <body>
+  <h1 class="title">タイトル</h1>
+  <p>URLを入力してください</p>
+  <input type="search" name="example2" size="30" maxlength="30" autofocus>
+  <button name="search" size="60" maxlength="30">検索</button>
+  <p>検索ボタンを押すと認証画面に移動します...</p>
+  <p>あいうえお</p>
+  </body>
+</html>
